c672d12b583d23760b1cd7503f3c20503aa369db
Saba-Baloch <156021281+Saba-Baloch@users.noreply.github.com>
2024-03-23 01:14:00 +0430

    added index.html file

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// <===========first question============>
+
+function percantage(om) {
+    return(om/1100*100);
+    
+}
+ let om =prompt("type your optain marks")
+ document.write(percantage(om))
---
+++ script1.js
@@ -0,0 +1,7 @@
+// <===============2nd question===========>
+function percantage(optainmarks,totalmarks) {
+    return(optainmarks/totalmarks*100);
+}
+let tm =prompt("type your total marks here⬇");
+let om =prompt("type your optain marks here⬇");
+document.write(percantage(tm,om));
